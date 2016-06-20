08a872df0df6bc769ae00ea89bfdb52053952a88
Aleksandra Kurek <kurek.aleksandra@gmail.com>
2016-06-20 22:24:44 +0200

    some cleaning

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
-// button scroll
-	jQuery(document).ready(function(){
+jQuery(document).ready(function() {
 
-		$("#button_about").click(function() {
-		    $('html, body').animate({
-		        scrollTop: $("#about").offset().top
-		    }, 1000);
-		});
+    // button scroll
+    $("#button_about").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 1000);
+    });
 
-		$("#button_portfolio").click(function() {
-				$('html, body').animate({
-						scrollTop: $("#portfolio").offset().top
-				}, 1000);
-		});
+    $("#button_portfolio").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#portfolio").offset().top
+        }, 1000);
+    });
 
     $("#button_contact").click(function() {
         $('html, body').animate({
@@ -21,21 +21,21 @@
 
 
 
-		// portfolio
-		$('.gallery ul li a').click(function() {
-				var itemID = $(this).attr('href');
-				$('.gallery ul').addClass('item_open');
-				$(itemID).addClass('item_open');
-				return false;
-		});
-		$('.close').click(function() {
-				$('.port, .gallery ul').removeClass('item_open');
-				return false;
-		});
+    // portfolio
+    $('.gallery ul li a').click(function() {
+        var itemID = $(this).attr('href');
+        $('.gallery ul').addClass('item_open');
+        $(itemID).addClass('item_open');
+        return false;
+    });
+    $('.close').click(function() {
+        $('.port, .gallery ul').removeClass('item_open');
+        return false;
+    });
 
-		$(".gallery ul li a").click(function() {
-				$('html, body').animate({
-						scrollTop: parseInt($("#top").offset().top)
-				}, 400);
-		});
+    $(".gallery ul li a").click(function() {
+        $('html, body').animate({
+            scrollTop: parseInt($("#top").offset().top)
+        }, 400);
+    });
 });
