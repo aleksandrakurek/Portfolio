41f65f5c0ae0da8b780872cc057b4008740dc48b
Aleksandra Kurek <kurek.aleksandra@gmail.com>
2016-06-20 09:53:19 +0200

    git push check

--- js/app.js
+++ js/app.js
@@ -18,4 +18,25 @@
             scrollTop: $("#contact").offset().top
         }, 1000);
     });
+
+
+
+		// portfolio
+		$('.gallery ul li a').click(function() {
+				var itemID = $(this).attr('href');
+				$('.gallery ul').addClass('item_open');
+				$(itemID).addClass('item_open');
+				return false;
+		});
+		$('.close').click(function() {
+				$('.port, .gallery ul').removeClass('item_open');
+				return false;
+		});
+
+		$(".gallery ul li a").click(function() {
+				$('html, body').animate({
+						scrollTop: parseInt($("#top").offset().top)
+				}, 400);
+		});
+
 });
