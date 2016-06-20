1c7518623bf85a51d144f18a3e625935bc6105ca
Aleksandra Kurek <kurek.aleksandra@gmail.com>
2016-06-20 08:38:42 +0200

    update in all sections

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// scroll do sekcji about me
+// button scroll
 	jQuery(document).ready(function(){
 
 		$("#button_about").click(function() {
@@ -6,6 +6,19 @@
 		        scrollTop: $("#about").offset().top
 		    }, 1000);
 		});
+
+		$("#button_skills").click(function() {
+				$('html, body').animate({
+						scrollTop: $("#skills").offset().top
+				}, 1000);
+		});
+
+		$("#button_portfolio").click(function() {
+				$('html, body').animate({
+						scrollTop: $("#portfolio").offset().top
+				}, 1000);
+		});
+
     $("#button_contact").click(function() {
         $('html, body').animate({
             scrollTop: $("#contact").offset().top
